879b2b03cedaf77f9112692c4110d51bffe19d5f
0xkongamoto <hieu.q@autonomous.nyc>
2022-03-29 18:36:39 +0700

    f

--- services/hooks.go
+++ services/hooks.go
@@ -1440,7 +1440,7 @@ func (s *NftLend) JobEvmNftypawnFilterLogs(ctx context.Context, network models.N
 					ctx,
 					models.NetworkMATIC,
 					uint64(resp.BlockNumber),
-					uint64(time.Now().Unix()),
+					uint64(resp.Timestamp),
 					resp.Hash,
 					resp.Index,
 					resp.Index,
