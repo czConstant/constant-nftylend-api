e0b29c1b8f7bc4a0ab981d4e9f262e6dc9556cd1
0xkongamoto <hieu.q@autonomous.nyc>
2022-05-31 11:11:51 +0700

    seen noti api

--- apis/notifications.go
+++ apis/notifications.go
@@ -19,3 +19,18 @@ func (s *Server) GetNotifications(c *gin.Context) {
 	}
 	ctxJSON(c, http.StatusOK, &serializers.Resp{Result: serializers.NewNotificationRespArr(notifications), Count: &count})
 }
+
+func (s *Server) SeenNotification(c *gin.Context) {
+	ctx := s.requestContext(c)
+	var req serializers.SeenNotificationReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		ctxJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
+		return
+	}
+	err := s.nls.SeenNotification(ctx, &req)
+	if err != nil {
+		ctxJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
+		return
+	}
+	ctxJSON(c, http.StatusOK, &serializers.Resp{Result: true})
+}
