f4867f11f852cde97b5ea5643ac9501422d53824
0xkongamoto <hieu.q@autonomous.nyc>
2022-06-02 17:24:18 +0700

    rf notification

--- services/notifications.go
+++ services/notifications.go
@@ -10,41 +10,57 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-func (s *NftLend) CreateNotification(ctx context.Context, network models.Network, notiType models.NotificationType, address string, reqMap map[string]interface{}) error {
-	notiTpl, err := s.ntd.First(
+func (s *NftLend) CreateNotification(ctx context.Context, network models.Network, address string, notiType models.NotificationType, reqMap map[string]interface{}) error {
+	err := daos.WithTransaction(
 		daos.GetDBMainCtx(ctx),
-		map[string][]interface{}{
-			"type = ?":    []interface{}{notiType},
-			"enabled = ?": []interface{}{true},
+		func(tx *gorm.DB) error {
+			user, err := s.getUser(tx, network, address)
+			if err != nil {
+				return errs.NewError(err)
+			}
+			notiTpl, err := s.ntd.First(
+				tx,
+				map[string][]interface{}{
+					"type = ?":    []interface{}{notiType},
+					"enabled = ?": []interface{}{true},
+				},
+				map[string][]interface{}{},
+				[]string{},
+			)
+			if err != nil {
+				return errs.NewError(err)
+			}
+			if notiTpl != nil {
+				noti, err := notiTpl.Execute(network, address, user.ID, reqMap)
+				if err != nil {
+					return errs.NewError(err)
+				}
+				s.nd.Create(
+					tx,
+					noti,
+				)
+				if err != nil {
+					return errs.NewError(err)
+				}
+			}
+			return nil
 		},
-		map[string][]interface{}{},
-		[]string{},
 	)
 	if err != nil {
 		return errs.NewError(err)
-	}
-	if notiTpl != nil {
-		noti, err := notiTpl.Execute(network, address, reqMap)
-		if err != nil {
-			return errs.NewError(err)
-		}
-		s.nd.Create(
-			daos.GetDBMainCtx(ctx),
-			noti,
-		)
-		if err != nil {
-			return errs.NewError(err)
-		}
 	}
 	return nil
 }
 
 func (s *NftLend) GetNotifications(ctx context.Context, network models.Network, address string, page int, limit int) ([]*models.Notification, uint, error) {
+	user, err := s.GetUser(ctx, network, address)
+	if err != nil {
+		return nil, 0, errs.NewError(err)
+	}
 	notifications, count, err := s.nd.Find4Page(
 		daos.GetDBMainCtx(ctx),
 		map[string][]interface{}{
-			"network = ?": []interface{}{network},
-			"address = ?": []interface{}{address},
+			"user_id = ?": []interface{}{user.ID},
 		},
 		map[string][]interface{}{},
 		[]string{"id desc"},
@@ -88,8 +104,7 @@ func (s *NftLend) SeenNotification(ctx context.Context, req *serializers.SeenNot
 			noti, err := s.nd.First(
 				tx,
 				map[string][]interface{}{
-					"network = ?": []interface{}{req.Network},
-					"address = ?": []interface{}{req.Address},
+					"user_id = ?": []interface{}{user.ID},
 				},
 				map[string][]interface{}{},
 				[]string{"id desc"},
