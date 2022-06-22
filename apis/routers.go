e3fbe843c031cd744cb130499bdac961bcc3d562
0xkongamoto <hieu.q@autonomous.nyc>
2022-06-22 13:50:15 +0700

    f

--- apis/routers.go
+++ apis/routers.go
@@ -129,5 +129,6 @@ func (s *Server) Routers() {
 	{
 		affiliateAPI.GET("/stats", s.GetAffiliateStats)
 		affiliateAPI.GET("/volumes", s.GetAffiliateVolumes)
+		affiliateAPI.GET("/transactions", s.GetAffiliateTransactions)
 	}
 }
